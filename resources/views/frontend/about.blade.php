e40f256dd2d1ae091a211a30a439fa4bc701d9ed
ferdyantorand <randf77@gmail.com>
2019-01-21 19:40:50 +0700

    About Mobile View

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -102,37 +102,46 @@
 
 @section('content')
 
+
     <!-- About -->
     <section class="bg-0 flex-wr-s-st m-b-15-sr991">
         <div class="col-md-12 size-a-18 bg-img2 respon-3 header-img" style="background-image: url('{{ asset('images/marc/about/Marcs Website components-15.jpg') }}');"></div>
     </section>
 
-    <!-- Why chosse us -->
-    <section class="bg-0 p-t-60 p-b-10">
+    <!-- Mobile Section Start -->
+    <section class="bg-0 p-t-60 p-b-10 d-block d-md-none">
         <div class="container">
             <!-- Title section -->
-            <div class="flex-col-c-c p-b-50">
+            <div class="flex-col-c-c p-b-0">
                 <h3 class="t1-b-1 cl-3 txt-center m-b-11 custom-font-2">
-                    BEGIN YOUR CHAPTER
+                    BEGIN
+                </h3>
+                <h3 class="t1-b-1 cl-3 txt-center m-b-11 custom-font-2">
+                    YOUR
+                </h3>
+                <h3 class="t1-b-1 cl-3 txt-center m-b-11 custom-font-2">
+                    CHAPTER
                 </h3>
                 <h5 class="t1-s-2 cl-3 txt-center m-b-11 custom-font-2" style="font-weight: 500;">
-                    "This is not only a home. This Home is a witness to
+                    "This is not only a home.
                     <br/>
-                    those who live their story."
+                    This Home is a witness to those
+                    <br/>
+                    who live their story."
                 </h5>
             </div>
         </div>
     </section>
 
-    <section class="bg-0 p-t-0 p-b-20">
+    <section class="bg-0 p-t-0 p-b-20 d-block d-md-none">
         <div class="container">
             <div class="flex-col-c-c p-b-20">
-                <div class="size-a-2 bg-3"></div>
+                <div class="size-a-2 bg-3" style="width: 50px;"></div>
             </div>
         </div>
     </section>
 
-    <section class="bg-0 p-t-10 p-b-10">
+    <section class="bg-0 p-t-10 p-b-10 d-block d-md-none">
         <div class="container">
             <!-- Title section -->
             <div class="row">
@@ -152,7 +161,236 @@
         </div>
     </section>
 
-    <section class="bg-0 p-t-0 p-b-20">
+    <section class="m-b-50 d-block d-md-none">
+        <div class="container-fluid px-0">
+            <!--  -->
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <div class="w-100 img-banner-responsive" style="background-image: url('{{ asset('images/marc/about/Marcs Website components-16.jpg') }}');
+                            background-repeat: no-repeat;
+                            background-position: center;
+                            background-size: cover;
+                            height: 600px;">
+                        <div class="box h-100 d-flex justify-content-center flex-column text-center">
+                            <div class="d-block d-md-none p-3">
+                                <h1 class="t1-b-1 white custom-font-2" style="text-align: center; vertical-align: center;">THE PRIDE<br/>WORTH ACHIEVING.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-10 p-b-10 d-block d-md-none">
+        <div class="container">
+            <!-- Title section -->
+            <div class="row">
+                <div class="col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11 custom-font-2" style="font-weight: 500; font-size: 20px;">
+                        "This is the home you will
+                        <br/>
+                        proud to own. This will be
+                        <br/>
+                        your pride."
+                    </h5>
+                </div>
+                <div class="col-12"></div>
+                <br/>
+                <div class="flex-col-c-c p-b-20 col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11">
+                        Wrapped around the dashingly designed facade
+                        are finely crafted living spaces and seamless blend
+                        of sleekly developed residences, retail, amenities
+                        and green open spaces. The highly sought-after
+                        location, along with its remarkable landmark
+                        architecture, lead Marc's Boulevard by Triniti Land
+                        to be a grand and exceptional living surrounding.
+                        Presenting a livable stage where those beautiful
+                        stories of yours will be made alive.
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-0 p-b-30 d-block d-md-none">
+        <div class="container">
+            <div class="flex-col-c-c p-b-20">
+                <div class="size-a-2 bg-3" style="width: 50px; height: 2px;"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="m-b-50 d-block d-md-none">
+        <div class="container-fluid px-0">
+            <!--  -->
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <div class="w-100 img-banner-responsive" style="background-image: url('{{ asset('images/marc/about/Marcs Website components-17.jpg') }}');
+                            background-repeat: no-repeat;
+                            background-position: center;
+                            background-size: cover;
+                            height: 600px;">
+                        <div class="box h-100 d-flex justify-content-center flex-column text-center">
+                            <div class="d-block d-md-none p-3">
+                                <h1 class="t1-b-1 white custom-font-2" style="text-align: center; vertical-align: center;">THE HOME<br/>WORTH LIVING.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-10 p-b-10 d-block d-md-none">
+        <div class="container">
+            <!-- Title section -->
+            <div class="row">
+                <div class="col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11 custom-font-2" style="font-weight: 500; font-size: 20px;">
+                        "This is the place you will love
+                        <br/>
+                        to stay. This will be the home
+                        <br/>
+                        you love."
+                    </h5>
+                </div>
+                <div class="col-12"></div>
+                <br/>
+                <div class="flex-col-c-c p-b-20 col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11">
+                        Find your most idealized expectation of a fulfilling life
+                        being satisfied in Marc's Boulevard by Triniti Land.
+                        Peaceful elegance by the green hues of nature mingles
+                        well with it's modern and aesthetically-pleasing design.
+                        Featuring extensive forms of life-enhancing facilities and
+                        communal space, Marc's Boulevard provides countless
+                        meaningful experiences and add a pleasing touch to
+                        every story you live in.
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-0 p-b-30 d-block d-md-none">
+        <div class="container">
+            <div class="flex-col-c-c p-b-20">
+                <div class="size-a-2 bg-3" style="width: 50px; height: 2px;"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="m-b-50 d-block d-md-none">
+        <div class="container-fluid px-0">
+            <!--  -->
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <div class="w-100 img-banner-responsive" style="background-image: url('{{ asset('images/marc/about/Marcs Website components-18.jpg') }}');
+                            background-repeat: no-repeat;
+                            background-position: center;
+                            background-size: cover;
+                            height: 600px;">
+                        <div class="box h-100 d-flex justify-content-center flex-column text-center">
+                            <div class="d-block d-md-none p-3">
+                                <h1 class="t1-b-1 white custom-font-2" style="text-align: center; vertical-align: center;">THE HOME<br/>WORTH LIVING.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-10 p-b-10 d-block d-md-none">
+        <div class="container">
+            <!-- Title section -->
+            <div class="row">
+                <div class="col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11 custom-font-2" style="font-weight: 500; font-size: 20px;">
+                        "This is the home where
+                        <br/>
+                        you choose to live. This is
+                        <br/>
+                        the Marc."
+                    </h5>
+                </div>
+                <div class="col-12"></div>
+                <br/>
+                <div class="flex-col-c-c p-b-20 col-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11">
+                        The fully integrated mixed-use development of Marc's
+                        Boulevard presents an utmost convenience to all its
+                        residents. Whether it be picking up your daughter from
+                        her first day of school or rewarding yourself at that newly
+                        opened flagship stores, nothing is far from your door.
+                        Initiated by an award-winning developer, Triniti Land,
+                        Each of these five unique districts of perfection are
+                        thoughtfully planned with a single-minded dedication to
+                        the highest standards of quality living spaces and life
+                        fulfillment.
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-0 p-b-30 d-block d-md-none">
+        <div class="container">
+            <div class="flex-col-c-c p-b-20">
+                <div class="size-a-2 bg-3" style="width: 50px; height: 2px;"></div>
+            </div>
+        </div>
+    </section>
+    <!-- Mobile Section Finish -->
+
+    <!-- Why chosse us -->
+    <section class="bg-0 p-t-60 p-b-10 d-none d-md-block">
+        <div class="container">
+            <!-- Title section -->
+            <div class="flex-col-c-c p-b-50">
+                <h3 class="t1-b-1 cl-3 txt-center m-b-11 custom-font-2">
+                    BEGIN YOUR CHAPTER
+                </h3>
+                <h5 class="t1-s-2 cl-3 txt-center m-b-11 custom-font-2" style="font-weight: 500;">
+                    "This is not only a home. This Home is a witness to
+                    <br/>
+                    those who live their story."
+                </h5>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-0 p-b-20 d-none d-md-block">
+        <div class="container">
+            <div class="flex-col-c-c p-b-20">
+                <div class="size-a-2 bg-3"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-10 p-b-10 d-none d-md-block">
+        <div class="container">
+            <!-- Title section -->
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="flex-col-c-c p-b-50 col-md-6 col-xs-12">
+                    <h5 class="t1-s-2 cl-3 txt-center m-b-11">
+                        Holding son for the first time, launching the very first business, meeting a blind date : the days are filled
+                        with stories. Big and small. To live in a place where those stories can spring is one start to begin with.
+                        Inspired by worth-living stories, Marc's Boulevard by Triniti Land is an admired development, with five
+                        stylish collections of unique districts adding fine touches to the horizon of Indonesia's thriving Batam City.
+                        It presents seamless living spaces which celebrate the pride worth achieving, the home worth loving and
+                        the Marc worth living. All for the greatest chapters of your life.
+                    </h5>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-0 p-t-0 p-b-20 d-none d-md-block">
         <div class="container">
             <div class="flex-col-c-c p-b-20">
                 <div class="size-a-2 bg-3" style="width: 100%; height: 2px;"></div>
@@ -160,7 +398,7 @@
         </div>
     </section>
 
-    <section class="bg-0 p-t-40 p-b-30">
+    <section class="bg-0 p-t-40 p-b-30 d-none d-md-block">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-sm-4 col-md-4 col-lg-4">
@@ -214,7 +452,7 @@
         </div>
     </section>
 
-    <section class="bg-0 p-t-0 p-b-20">
+    <section class="bg-0 p-t-0 p-b-20 d-none d-md-block">
         <div class="container">
             <div class="flex-col-c-c p-b-20">
                 <div class="size-a-2 bg-3" style="width: 100%; height: 2px;"></div>
@@ -222,7 +460,7 @@
         </div>
     </section>
 
-    <section class="bg-0 p-t-40 p-b-30">
+    <section class="bg-0 p-t-40 p-b-30 d-none d-md-block">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-sm-4 col-md-4 col-lg-4 mycontent-left">
@@ -293,7 +531,7 @@
         </div>
     </section>
 
-    <section class="bg-0 p-t-0 p-b-20">
+    <section class="bg-0 p-t-0 p-b-20 d-none d-md-block">
         <div class="container">
             <div class="flex-col-c-c p-b-20">
                 <div class="size-a-2 bg-3" style="width: 100%; height: 2px;"></div>
