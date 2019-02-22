e68e84bdd30ee63ff21bcca9d75f9e7e5b8bacb2
willpasc <williampascal8@gmail.com>
2019-02-22 17:01:58 +0700

    Home Banner

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -11,7 +11,282 @@
 @section('content')
 
     <!-- Banner -->
-    <section class="m-b-50">
+
+    <section class="slider">
+        <div class="rev_slider_wrapper fullwidthbanner-container">
+            <div id="rev_slider_1" class="rev_slider fullwidthabanner" data-version="5.4.5" style="display:none">
+                <ul>
+                    <!-- Slide 1 -->
+                    <li data-transition="slidingoverlayhorizontal">
+                        <img src="images/marc/home/home-1.jpg" alt="IMG-SLIDE" class="rev-slidebg">
+
+                        <h2 class="tp-caption tp-resizeme caption-1 text-uppercase" 
+                        data-frames='[{"delay":500,"speed":1500,"frame":"0","from":"x:left;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['48', '48', '48', '38']" 
+                        data-lineheight="['58', '58', '58', '58']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-83', '-83', '-83', '-93']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >BEGIN YOUR CHAPTER</h2>
+
+                        <p class="tp-caption tp-resizeme caption-2" 
+                        data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"x:right;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['30', '30', '30', '25']" 
+                        data-lineheight="['39', '39', '39', '39']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-13', '-13', '-13', '-13']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                        <Span>This is not only a home. This Home is a witness to</span>
+                        <span>those who live their story.<span>
+                        </p>
+
+                        {{-- <div class="tp-caption tp-resizeme caption-3 flex-wr-c-c d-flex"
+                        data-frames='[{"delay":3000,"speed":1500,"frame":"0","from":"y:bottom;rX:-20deg;rY:-20deg;rZ:0deg;","to":"o:1;","ease":"Power3.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['88', '88', '88', '88']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[10, 10, 10, 10]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[10, 10, 10, 10]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                            <a href="projects-grid.html" class="btn1 flex-c-c">
+                                Our project
+                            </a>
+
+                            <a href="about.html" class="btn2 flex-c-c">
+                                Learn more
+                            </a>
+                        </div> --}}
+                    </li>
+
+                    <!-- Slide 2 -->
+                    <li data-transition="slidingoverlayvertical">
+                        <img src="images/marc/home/home-2.jpg" alt="IMG-SLIDE" class="rev-slidebg">
+
+                        <h2 class="tp-caption tp-resizeme caption-1 text-uppercase" 
+                        data-frames='[{"delay":500,"speed":1500,"frame":"0","from":"y:top;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['48', '48', '48', '38']" 
+                        data-lineheight="['58', '58', '58', '58']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-83', '-83', '-83', '-93']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >BEGIN YOUR CHAPTER</h2>
+
+                        <p class="tp-caption tp-resizeme caption-2" 
+                        data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"y:bottom;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['30', '30', '30', '25']" 
+                        data-lineheight="['39', '39', '39', '39']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-13', '-13', '-13', '-13']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                        This is not only a home. This Home is a witness to
+                        those who live their story.
+                        </p>
+
+                        {{-- <div class="tp-caption tp-resizeme caption-3 flex-wr-c-c d-flex"
+                        data-frames='[{"delay":3000,"speed":1500,"frame":"0","from":"z:0;rX:0;rY:0;rZ:0;sX:0.8;sY:0.8;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['88', '88', '88', '88']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[10, 10, 10, 10]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[10, 10, 10, 10]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                            <a href="projects-grid.html" class="btn1 flex-c-c">
+                                Our project
+                            </a>
+
+                            <a href="about.html" class="btn2 flex-c-c">
+                                Learn more
+                            </a>
+                        </div> --}}
+                    </li>
+
+                    <!-- Slide 3 -->
+                    <li data-transition="boxslide">
+                        <img src="images/marc/home/home-4.jpg" alt="IMG-SLIDE" class="rev-slidebg">
+
+                        <h2 class="tp-caption tp-resizeme caption-1 text-uppercase" 
+                        data-frames='[{"delay":500,"speed":1500,"frame":"0","from":"x:-500px;skX:85px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['48', '48', '48', '38']" 
+                        data-lineheight="['58', '58', '58', '58']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-83', '-83', '-83', '-93']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >BEGIN YOUR CHAPTER</h2>
+
+                        <p class="tp-caption tp-resizeme caption-2" 
+                        data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"x:500px;skX:-85px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-visibility="['on', 'on', 'on', 'on']"
+ 
+                        data-fontsize="['30', '30', '30', '25']" 
+                        data-lineheight="['39', '39', '39', '39']"
+                        data-color="['#FFF']" 
+                        data-textAlign="['center', 'center', 'center', 'center']"
+                     
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['-13', '-13', '-13', '-13']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto', 'auto', 'auto', 'auto']" 
+                        data-whitespace="['normal']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[15, 15, 15, 15]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[15, 15, 15, 15]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                        This is not only a home. This Home is a witness to
+                        those who live their story.
+                        </p>
+
+                        {{-- <div class="tp-caption tp-resizeme caption-3 flex-wr-c-c d-flex"
+                        data-frames='[{"delay":3000,"speed":1500,"frame":"0","from":"y:bottom;rZ:90deg;sX:2;sY:2;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                        data-x="['center']" 
+                        data-y="['center']" 
+                        data-hoffset="['0', '0', '0', '0']" 
+                        data-voffset="['88', '88', '88', '88']" 
+                     
+                        data-width="['1200','992','768','480']"
+                        data-height="['auto']" 
+                     
+                        data-paddingtop="[0, 0, 0, 0]"
+                        data-paddingright="[10, 10, 10, 10]"
+                        data-paddingbottom="[0, 0, 0, 0]"
+                        data-paddingleft="[10, 10, 10, 10]"
+
+                        data-basealign="slide" 
+                        data-responsive_offset="off"
+                        >
+                            <a href="projects-grid.html" class="btn1 flex-c-c">
+                                Our project
+                            </a>
+
+                            <a href="about.html" class="btn2 flex-c-c">
+                                Learn more
+                            </a>
+                        </div> --}}
+                    </li>
+                </ul>				
+            </div>
+        </div>
+    </section>
+    {{-- <section class="m-b-50">
 
         <div class="container-fluid px-0">
             <!--  -->
@@ -45,7 +320,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <!-- Paul Marc Desktop -->
     <section class="m-b-50 d-none d-md-block">
